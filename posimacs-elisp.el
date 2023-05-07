adee45f6ade504e17282a5916e20c64f95f51c7f
Psionik K <73710933+psionic-k@users.noreply.github.com>
2023-05-07 00:50:38 -0500

    Move ielm hacking to elisp module
    Definitely about improving the elisp experience

--- posimacs-elisp.el
+++ posimacs-elisp.el
@@ -2,12 +2,13 @@
 
 ;;; Commentary:
 ;;
-;; A completion framework and sources of suggestions to populate it are the most
-;; essential behavior to enable in Emacs.  It makes discovering the rest of
-;; Emacs as easy as searching, frequently adding additional context to the
-;; results such as displaying hotkeys or docstrings, making all workflows become
-;; extremely fast with less time spent reading documentation.
+;; The out-of-box experience with elisp is extremely conservative.  Editing,
+;; introspecting, and maintaining projects should all be things Emacs is very
+;; competent at.  While experienced users may opt for different or less things,
+;; it's unreasonable to assume unfamiliar Emacs users want a bare bones
+;; experience.
 ;;
+;; Some pure-style configurations can be found in posimacs-style.el
 
 ;;; Code:
 
@@ -15,10 +16,72 @@
   :elpaca nil
   :config
   ;; When the output is huge, ielm can slow down.  Instead, let's just get rid
-  ;; of large output since there are other ways to handle this better.
-  (setq-local comint-max-line-length 1024)
-  (declare-function comint-truncate-buffer "comint.el")
-  (unless (memq #'comint-truncate-buffer comint-output-filter-functions)
-    (push #'comint-truncate-buffer comint-output-filter-functions)))
+  ;; of large output since there are other ways to handle this better and
+  ;; usually the user will change approach when the output is impractical.
+  (add-hook #'inferior-emacs-lisp-mode
+            (lambda () (setq-local comint-max-line-length 1024)
+              ;; comint-output-filter-functions is permanent local
+              (unless (memq #'comint-truncate-buffer comint-output-filter-functions)
+                (push #'comint-truncate-buffer comint-output-filter-functions)))))
 
+;; pmx-ielm-this-buffer is a shortcut to open an ielm buffer for introspective
+;; hacking on that buffer.  This is often faster than the scratch buffer
+;; workflow, which both accumulates broken forms and requires manually
+;; specifying which buffer you want to operate on.
+(defun pmx--mru-buffer ()
+  "Get the buffer of the most recently used window."
+  (window-buffer (get-mru-window t t t)))
+
+(defun pmx--not-ielm-buffer-p (buffer)
+  "Filter predicate to remove ielm buffers from `read-buffer' candidates.
+
+BUFFER candidate"
+  (not (eq (buffer-local-value 'major-mode buffer)
+           'inferior-emacs-lisp-mode)))
+
+(defun pmx-ielm-this-buffer (&optional target-buffer)
+  "Hack this buffer's locals with elisp in ielm.
+
+TARGET-BUFFER buffer whose locals we want to hack
+
+If the TARGET-BUFFER is already an ielm buffer, warn instead.
+When TARGET-BUFFER is nil, use the result of `current-buffer'"
+  (interactive)
+  (let* ((target-buffer (or target-buffer (current-buffer)))
+        (new-buffer-name (format "*ielm-<%s>*" target-buffer)))
+    (if (pmx--not-ielm-buffer-p target-buffer)
+      (progn (pop-to-buffer (get-buffer-create new-buffer-name))
+             (ielm new-buffer-name)
+             (ielm-change-working-buffer target-buffer))
+      (user-error "Target buffer %s is already an ielm buffer" (buffer-name target-buffer)))))
+
+(global-set-key (kbd "M-i") 'pmx-ielm-this-buffer)
+
+;; Surprised this isn't in core already
+;; https://emacs.stackexchange.com/questions/24657/unadvise-a-function-remove-all-advice-from-it
+(defun pmx-advice-unadvice (sym)
+  "Remove all advices from symbol SYM."
+  (interactive "aFunction symbol: ")
+  (advice-mapc (lambda (advice _props) (advice-remove sym advice)) sym))
+
+(defun pmx-dump-object-string (obj)
+  "Return a string contents of slots of OBJ."
+  (mapconcat
+   (lambda (i) (format "%s: %s" i
+                  (if (slot-boundp obj i)
+                      (eieio-oref obj i)
+                    eieio--unbound)))
+   (object-slots obj) "\n"))
+
+;; TODO Requires dash
+(defun pmx-dump-object (obj)
+  "Return a plist of slot-value pairs of eieio OBJ."
+   (->>
+    (object-slots obj)
+    (--map (list it (if (slot-boundp obj it)
+                        (eieio-oref obj it)
+                      eieio--unbound)))
+    (-concat)))
+
+(provide 'posimacs-elisp)
 ;;; posimacs-elisp.el ends here
--- posimacs-extras.el
+++ posimacs-extras.el
@@ -2,9 +2,7 @@
 
 ;;; Commentary:
 ;;
-;; Doom has a nice comprehensive theme system and lots of good starting points.
-;; Let's adopt!
-;;
+;; Packages and functions which are at best fun or useful on rare occasions.
 
 ;;; Code:
 
@@ -13,7 +11,6 @@
 
 (use-package snow
   :elpaca (snow :host github :repo "alphapapa/snow.el"))
-
 
 ;; tracking command usage to measure pain
 (use-package keyfreq
@@ -40,59 +37,9 @@ Saves to a temp file and puts the filename in the kill ring."
     (kill-new filename)
     (message filename)))
 
-
-;; Surprised this isn't in core already
-;; https://emacs.stackexchange.com/questions/24657/unadvise-a-function-remove-all-advice-from-it
-(defun pmx-advice-unadvice (sym)
-  "Remove all advices from symbol SYM."
-  (interactive "aFunction symbol: ")
-  (advice-mapc (lambda (advice _props) (advice-remove sym advice)) sym))
-
-(defun pmx-dump-object-string (obj)
-  "Return a string contents of slots of an object."
-  (mapconcat
-   (lambda (i) (format "%s: %s" i
-                  (if (slot-boundp obj i)
-                      (eieio-oref obj i)
-                    eieio--unbound)))
-   (object-slots obj) "\n"))
-
-(defun pmx-dump-object (obj)
-  "Return a plist of slot-value pairs of eieio object."
-   (->>
-    (object-slots obj)
-    (--map (list it (if (slot-boundp obj it)
-                        (eieio-oref obj it)
-                      eieio--unbound)))
-    (-concat)))
-
-(defun pmx--mru-buffer ()
-  "Get the buffer of the most recently used window."
-  (window-buffer (get-mru-window t t t)))
-
-(defun pmx--not-ielm-buffer-p (buffer)
-  "Filter predicate to remove ielm buffers from `read-buffer' candidates.
-
-BUFFER candidate"
-  (not (eq (buffer-local-value 'major-mode buffer)
-           'inferior-emacs-lisp-mode)))
-
-(defun pmx-ielm-this-buffer (&optional target-buffer)
-  "Hack this buffer's locals with elisp in ielm.
-
-TARGET-BUFFER buffer whose locals we want to hack
-
-If the TARGET-BUFFER is already an ielm buffer, warn instead.
-When TARGET-BUFFER is nil, use the result of `current-buffer'"
   (interactive)
-  (let* ((target-buffer (or target-buffer (current-buffer)))
-        (new-buffer-name (format "*ielm-<%s>*" target-buffer)))
-    (if (pmx--not-ielm-buffer-p target-buffer)
-      (progn (pop-to-buffer (get-buffer-create new-buffer-name))
-             (ielm new-buffer-name)
-             (ielm-change-working-buffer target-buffer))
-      (user-error "Target buffer %s is already an ielm buffer" (buffer-name target-buffer)))))
 
 
 
+(provide 'posimacs-extras)
 ;;; posimacs-extras.el ends here
