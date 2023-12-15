4f09ee1992d6e419f6701c03ed0643f7832aa279
Psionik K <73710933+psionic-k@users.noreply.github.com>
2023-12-15 10:38:04 +0900

    rust-ts-mode
    Signed-off-by: Psionik K <73710933+psionic-k@users.noreply.github.com>

--- rust/posimacs-rust.el
+++ rust/posimacs-rust.el
@@ -5,15 +5,21 @@
 
 ;;; Code:
 
-(use-package rustic
-  :custom
-  (rustic-spinner-type "moon")
-  :config
-  (setq rustic-lsp-server 'rust-analyzer)
-  (setq rustic-analyzer-command '("rust-analyzer"))
-  (setenv "RUST_BACKTRACE" "full")
+;; Tree sitter support is installed 😀
+;; TODO This lisp is dependent upon Emacs being installed with tree sitter
+;; support, which is in ../posimacs.nix
+;; It is a dependency, and stating it within the Rust module would require some
+;; option support, from the top-level module, and modules add complexity...
+;; Possibly we can configure Emacs to load the tree sitter grammars without
+;; having them in the Emacs derivation itself.
 
-  ;; comment to disable rustfmt on save
-  (setq rustic-format-on-save t))
+(setq major-mode-remap-alist
+      '((rust-mode . rust-ts-mode)))
+
+(use-package rust-ts-mode
+  :hook (rust-ts-mode . eglot-ensure)
+  :elpaca nil
+  :init
+  (setenv "RUST_BACKTRACE" "full"))
 
 ;;; posimacs-rust.el ends here.
