e68500a1ee7a5956be433b18e87f9e8005d32a39
Psionik K <73710933+psionic-k@users.noreply.github.com>
2024-01-07 23:27:51 +0900

    A very nice augmentation to Eat
    Signed-off-by: Psionik K <73710933+psionic-k@users.noreply.github.com>

--- posimacs-terminal.el
+++ posimacs-terminal.el
@@ -7,9 +7,21 @@
 ;;; Code:
 
 (use-package eat
+  :after avy
   :config
   (add-hook 'eat-session-created-hook #'eat-semi-char-mode)
   (keymap-set eat-semi-char-mode-map "M-o" nil)
+  (defun pmx--eat-jump-to-line ()
+    (interactive)
+    (eat-line-mode)
+    (call-interactively #'avy-goto-word-1))
+  (keymap-set eat-semi-char-mode-map "M-j" #'pmx--eat-jump-to-line)
+
+  (defun pmx--eat-return-to-semi-char ()
+    (interactive)
+    (eat-semi-char-mode)
+    (goto-char (point-max)))
+  (keymap-set eat-line-mode-map "RET" #'pmx--eat-return-to-semi-char)
   (define-obsolete-function-alias 'vterm #'eat "0.1.0"))
 
 (provide 'posimacs-terminal)
