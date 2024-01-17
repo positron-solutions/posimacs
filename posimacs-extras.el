3c539da2e7a5c2b07caeffaf4e5ee1b5ec70c98e
Psionik K <73710933+psionic-k@users.noreply.github.com>
2024-01-17 15:21:24 +0900

    Mandelbrot.  What a great package
    Signed-off-by: Psionik K <73710933+psionic-k@users.noreply.github.com>

--- posimacs-extras.el
+++ posimacs-extras.el
@@ -12,6 +12,8 @@
 (use-package snow
   :elpaca (snow :host github :repo "alphapapa/snow.el"))
 
+(use-package mandelbrot)
+
 ;; tracking command usage to measure pain
 (use-package keyfreq
   :init
@@ -116,29 +118,6 @@ Saves to a temp file and puts the filename in the kill ring."
       (insert data))
     (kill-new filename)
     (message filename)))
-
-;;;###autoload
-(defun pmx-mandelbrot ()
-  "Render a mandelbrot."
-  (interactive)
-  (pop-to-buffer (get-buffer-create "*mandelbrot*"))
-  (let ((w 800) (h 720) (d 64))
-    (fundamental-mode) (erase-buffer)
-    (set-buffer-multibyte nil)
-    (insert (format "P6\n%d %d\n255\n" w h))
-    (dotimes (y h)
-      (dotimes (x w)
-        (let ((cx (* 1.5 (/ (- x (/ w 1.45)) w 0.45)))
-               (cy (* 1.5 (/ (- y (/ h 2.0)) h 0.5)))
-               (zr 0)
-               (zi 0))
-          (let ((v (cl-dotimes (v d d)
-                    (if (> (+ (* zr zr) (* zi zi)) 4) (cl-return v)
-                      (cl-psetq
-                       zr (+ (* zr zr) (- (* zi zi)) cx)
-                       zi (+ (* (* zr zi) 2) cy))))))
-            (insert-char (floor (* 256 (/ v 1.0 d))) 3)))))
-    (image-mode)))
 
 (provide 'posimacs-extras)
 ;;; posimacs-extras.el ends here
