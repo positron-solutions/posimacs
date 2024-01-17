590b86cf0fe26601f9f6fc745da26f848fa19a49
Psionik K <73710933+psionic-k@users.noreply.github.com>
2024-01-17 15:18:40 +0900

    watching my garbage collection a bit more
    Because the idle GC threshold is really low, we can use a high percentage.
    Conversely, during normal use, this means we rarely gc during interactive use.
    Overall seems fine.  Fewer huge pauses during GC's.
    
    Signed-off-by: Psionik K <73710933+psionic-k@users.noreply.github.com>

--- posimacs-init.el
+++ posimacs-init.el
@@ -120,6 +120,10 @@
 (use-package gcmh
   :demand t
   :config
+  ;; (setopt garbage-collection-messages t)
+  (setopt gcmh-high-cons-threshold (* 128 800000))
+  (setopt gcmh-low-cons-threshold (* 16 800000))
+  (setopt gc-cons-percentage 0.7)
   (add-hook 'elpaca-after-init-hook (lambda () (gcmh-mode 1))))
 
 ;;; Load modules.
