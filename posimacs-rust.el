e7ad97430e13d300a7c54efbd6291a6baf28cc4a
Psionik K <73710933+psionic-k@users.noreply.github.com>
2025-01-06 18:31:44 +0900

    Guess we're back to eglot for now

--- posimacs-rust.el
+++ posimacs-rust.el
@@ -12,11 +12,27 @@
 
 (use-package rust-mode
   :hook ((rust-mode .  electric-pair-local-mode)
-         (rust-mode .  lsp-deferred))
+         ;; (rust-mode .  lsp-deferred)
+         (rust-mode . eglot-ensure)
+         )
   :init
   (setenv "RUST_BACKTRACE" "full")
   (setq rust-mode-treesitter-derive t)
   :config
+
+  (setq eldoc-echo-area-prefer-doc-buffer 'maybe)
+  (setq eldoc-echo-area-use-multiline-p nil)
+  (setq echo-area-display-truncation-message nil)
+
+  ;; (setopt lsp-rust-analyzer-display-lifetime-elision-hints-enable "skip_trivial")
+  ;; (setopt lsp-rust-analyzer-display-chaining-hints t)
+  ;; (setopt lsp-rust-analyzer-display-lifetime-elision-hints-use-parameter-names nil)
+  ;; (setopt lsp-rust-analyzer-display-closure-return-type-hints t)n
+
+  (defun pmx--rust-tweaks ()
+    (auto-fill-mode -1)
+    (electric-pair-local-mode -1))
+  (add-hook 'rust-mode-hook #'pmx--rust-tweaks)
   (setq rust-format-on-save t)
   (defun pmx--rustfmt-use-leptos (orig-func &rest r)
     (if-let* ((leptosfmt (executable-find "leptosfmt")))
