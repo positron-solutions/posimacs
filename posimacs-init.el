6166093e30b4d5d5be4ae5791d1780ca5c2dd806
Psionik K <73710933+psionic-k@users.noreply.github.com>
2025-08-10 18:39:02 +0900

    good bye gcmh

--- posimacs-init.el
+++ posimacs-init.el
@@ -127,17 +127,6 @@
             (makunbound 'file-name-handler-alist--old))))
   (add-hook 'elpaca-after-init-hook pmx-clean-up))
 
-;; Idle garbage collection
-(use-package gcmh
-  :config
-  ;; (setopt garbage-collection-messages t)
-  (setopt gcmh-high-cons-threshold (* 512 1000 1000))
-  (setopt gcmh-low-cons-threshold (* 16 1000 1000))
-  (setopt gcmh-idle-delay 3)
-  ;; (setopt gcmh-verbose t)
-  (setopt gc-cons-percentage 0.2)
-  (add-hook 'elpaca-after-init-hook (lambda () (gcmh-mode 1))))
-
 ;;; Load modules.
 (let ((posimacs-files '("posimacs-defaults.el"
                         "posimacs-windows.el"
